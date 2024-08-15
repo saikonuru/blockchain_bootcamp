38a8899e478730c8dc3760eff6f4f49fc38e14ae
saikonuru <sairam.konuru@gmail.com>
2024-08-15 21:44:24 +0530

    ERC Demo

---
+++ contracts/ERC/GLDToken.sol
@@ -0,0 +1,14 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract CTEToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("GLDToken", "GLD") {
+        _mint(msg.sender, initialSupply);
+    }
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+}
